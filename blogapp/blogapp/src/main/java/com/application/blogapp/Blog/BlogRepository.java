0bf0f399fa6935cf2f1d8c38fa6bb4bb35a7cee9
Dachi abashidze <dachiabashidze@icloud.com>
2025-05-31 19:57:42 +0400

    reverted last commit

--- blogapp/blogapp/src/main/java/com/application/blogapp/Blog/BlogRepository.java
+++ blogapp/blogapp/src/main/java/com/application/blogapp/Blog/BlogRepository.java
@@ -1,6 +1,7 @@
 package com.application.blogapp.Blog;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface BlogRepository extends CrudRepository<Blog,String> {
+public interface BlogRepository extends JpaRepository<Blog,String> {
 }
